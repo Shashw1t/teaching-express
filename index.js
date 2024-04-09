744516a8299029f528e319e7ba3d4a4221a487dc
tncts <shashwat.satvik@gmail.com>
2024-04-10 03:37:16 +0530

    intial commit part 2

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
-const port = 3000
+const port = 8000
+
+app.use(bodyParser.json({}));
 
 app.get('/', (req, res) => {
+  console.log(req.body);
   res.send('Hello World!')
 })
 
