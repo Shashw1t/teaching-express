0724d13ad279d97d30d879a7630cc4eee90138de
tncts <shashwat.satvik@gmail.com>
2024-04-10 17:06:09 +0530

    5pm commit on 10th april

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = 8000
 
-app.use(bodyParser.json({}));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  console.log(req.body);
+app.post('/', (req, res) => {
+  // console.log(req.body);
   res.send('Hello World!')
 })
 
