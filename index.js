e4bcf8f8ac64a667ec263cf42040b48e66040887
tncts <shashwat.satvik@gmail.com>
2024-04-16 23:41:59 +0530

    all methods

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = 3000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Route handler for GET request
 app.get('/', (req, res) => {
-  res.send('Hello, junior'); 
+  res.send('GET request received');
 });
 
+// Route handler for POST request
+app.post('/', (req, res) => {
+  res.send('POST request received');
+});
+
+// Route handler for PUT request
+app.put('/', (req, res) => {
+  res.send('PUT request received');
+});
+
+// Route handler for DELETE request
+app.delete('/', (req, res) => {
+  res.send('DELETE request received');
+});
+
+// Route handler for PATCH request
+app.patch('/', (req, res) => {
+  res.send('PATCH request received');
+});
+
+// Route handler for OPTIONS request
+app.options('/', (req, res) => {
+  res.send('OPTIONS request received');
+});
+
+// Route handler for HEAD request
+app.head('/', (req, res) => {
+  res.send('HEAD request received');
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
