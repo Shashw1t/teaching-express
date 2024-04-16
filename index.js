c2d8c56e147dddbbe133defff3f058e53d430238
tncts <shashwat.satvik@gmail.com>
2024-04-16 22:01:53 +0530

    teaching more express

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-/* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
-const express = require('express')
-const bodyParser = require('body-parser');
-const app = express()
-const port = 8000
-
-app.use(bodyParser.json());
-
-app.post('/', (req, res) => {
-  // console.log(req.body);
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const express = require ('express');
