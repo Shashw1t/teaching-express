abf48bfd6e2a3fca146b2eec7f396db5fc22a9d8
tncts <shashwat.satvik@gmail.com>
2024-04-20 18:05:57 +0530

    explained middlewares

---
+++ index1.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+let numberOfRequest = 0;
+function calculateRequests(req,res,next){
+    numberOfRequest++;
+    console.log(numberOfRequest);
+    next();
+}
+
+app.get("/health-checkup", (req,res) =>{
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyid = req.query.kidneyid;
+
+    if(username != "shashwat" || password != "pass"){
+        res.status(400).json({"msg": "somethings up with your inputs"})
+        return
+    }
+    
+    if(kidneyid != 1 && kidneyid != 2){
+        res.status(400).json({"msg": "somethings up with your inputs"})
+        return
+    }
+
+    // do something with kidney here    
+    res.json({
+        msg: "your kidney is fine"
+    })
+});
+
+app.listen(3000);
