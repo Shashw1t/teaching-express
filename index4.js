503d4774030bbd284ae2b2f4ef5eac526839963b
tncts <shashwat.satvik@gmail.com>
2024-04-21 05:40:38 +0530

    zod introduction

---
+++ index4.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const zod = require('zod');
+const app = express();
+
+const schema = zod.array(zod.number());
+
+// {
+    //  email: string => email
+    // password: atleast 8 letters
+    // country : "IN"  , "US"
+// }
+
+const schema = zod.object({
+    email: zod.string(),
+    password: zod.string(),
+    country: zod.literal("IN").or(zod.literal("US"))
+})
+
+app.use(express.json());
+
+app.post("/health-checkup", (req,res) =>{
+    // kidneys = [1,2]
+    const kidneys = req.body.kidneys;
+    const response = schema.safeParse(kidneys);
+
+    if(!response.success){
+        res.status(411).json({
+            msg : `input is invalid`
+        })
+    } else{   
+        res.send({
+            response
+        })
+    }
+});
+ 
+app.listen(3000);
