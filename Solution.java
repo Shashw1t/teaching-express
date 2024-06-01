b59a469d819f427abe9a73090ebbe5e11c7074c4
tncts <shashwat.satvik@gmail.com>
2024-06-01 23:54:06 +0530

    random

--- Solution.java
+++ Solution.java
@@ -12,7 +12,7 @@ public class Solution {
             PoisonTime = PoisonTime + Math.min(timeDifference, duration);
         }
         
-        // Add the duration for the last 
+        // Add the duration for the 
         PoisonTime = PoisonTime + duration;
         return PoisonTime;
         }
