1e57ad2bd75241c6339f5a57f8cb0dd75f979ee3
tncts <shashwat.satvik@gmail.com>
2024-06-01 23:53:23 +0530

    random

---
+++ Solution.java
@@ -0,0 +1,19 @@
+import javax.swing.text.Position;
+
+public class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        
+        int PoisonTime = 0;
+        
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+            // Calculate the time difference between consecutive attacks
+            int timeDifference = timeSeries[i + 1] - timeSeries[i];
+            // Add the minimum of the time difference or the full duration
+            PoisonTime = PoisonTime + Math.min(timeDifference, duration);
+        }
+        
+        // Add the duration for the last attack
+        PoisonTime = PoisonTime + duration;
+        return PoisonTime;
+        }
+    }
