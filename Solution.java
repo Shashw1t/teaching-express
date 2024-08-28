d7601c19f4c35c426d5f76ec23e03bb70a45c4f4
Shashwat Dwivedi <122317691+Shashw1t@users.noreply.github.com>
2024-08-28 22:15:06 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -6,13 +6,10 @@ public class Solution {
         int PoisonTime = 0;
         
         for (int i = 0; i < timeSeries.length - 1; i++) {
-            // Calculate the time difference between consecutive attacks
             int timeDifference = timeSeries[i + 1] - timeSeries[i];
-            // Add the minimum of the time difference or the full duration
             PoisonTime = PoisonTime + Math.min(timeDifference, duration);
         }
         
-        // Add the 
         PoisonTime = PoisonTime + duration;
         return PoisonTime;
         }
