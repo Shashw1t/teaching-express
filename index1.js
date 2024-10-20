4861bbba43f03fd8e1c854e40e3e9e0401acc491
Shashwat Dwivedi <122317691+Shashw1t@users.noreply.github.com>
2024-10-20 23:35:13 +0530

    health checkup

--- index1.js
+++ index1.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const app = express();
-
 app.use(express.json());
 
 let numberOfRequest = 0;
@@ -11,7 +9,7 @@ function calculateRequests(req,res,next){
     next();
 }
 app.get("/health-checkup", (req,res) =>{
-    const username = req.headers.username;
+    const username = req.headers.username; // params seekhna
     const password = req.headers.password;
     const kidneyid = req.query.kidneyid;
 
@@ -21,7 +19,7 @@ app.get("/health-checkup", (req,res) =>{
     }
     
     if(kidneyid != 1 && kidneyid != 2){
-        res.status(400).json({"msg": "somethings up with your inputs"})
+        res.status(400).json({"msg": "somethings up with your inputs"}) // invalid input de rhe hain
         return
     }
 
@@ -31,4 +29,4 @@ app.get("/health-checkup", (req,res) =>{
     })
 });
 
-app.listen(3000);
+app.listen(3000); // this line tells which port number to listen to
